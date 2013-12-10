488bb60ea4bb87f10323db5632dcc80491ace633
Ryan Maciel <rmepicwin@gmail.com>
2013-12-10 14:14:14 -0500

    Commit 1 12/10/13

---
+++ OOS Mobile/Images.xcassets/OOSMDataHandlerModel.h
@@ -0,0 +1,13 @@
+//
+//  OOSMDataHandlerModel.h
+//  OOS Mobile
+//
+//  Created by Ryan Maciel on 12/10/13.
+//  Copyright (c) 2013 RPS ASA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OOSMDataHandlerModel : NSObject
+
+@end

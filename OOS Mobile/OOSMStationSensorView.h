77b080958a3ffbe3ccc65a167357b55515b812db
RyanMaciel <rspams1234@gmail.com>
2014-04-30 17:12:58 -0400

    Added units to the labels on the station view and fixed the way their labels behave. Integrated the graph into the UI, which resolves asascience-open/oos-mobile#14 and  resolves asascience-open/oos-mobile#16.

--- OOS Mobile/OOSMStationSensorView.h
+++ OOS Mobile/OOSMStationSensorView.h
@@ -20,10 +20,10 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <UIKit/UIKit.h>
-
 @interface OOSMStationSensorView : UIView
-@property(strong, nonatomic)NSString *sensorIconImageName;
-@property(strong, nonatomic)NSString *sensorPropertyValue;
+@property(strong, nonatomic)NSString *propertyObserved;
+@property(weak, nonatomic)UIViewController *stationInfoViewController;
+
 -(id)initWithFrame:(CGRect)frame
     sensorIconName:(NSString*)sensorIconName
 sensorPropertyValue:(NSString*)sensorPropertyValue;
